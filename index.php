4d6b0d677a4246ff3bed25c35d0bd98bc8d96cb9
Rob Lovelett <rlovelett@gmail.com>
2016-10-31 21:13:38 -0500

    updated index.php to be responsive

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Basic Portfolio: Robert Lovelett</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+        <div class="header">
+            <div class="container">
+                <div class="row">
+                    <div class="header-name">Robert Lovelett</div>
+                    <div class="header-nav">
+                        <nav>
+                            <ul>
+                                <li><a href="index.php">About</a></li>
+                                <li><a href="portfolio.php">Portfolio</a></li>
+                                <li><a href="contact.php">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="u-cf"></div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="main">
+                        <article>
+                            <h1>About Me</h1>
+                            <hr />
+                            <img src="assets/images/profile.png" alt="Robert Lovelett" class="profile" />
+                            <p>My name is Robert Lovelett and I am an animator, illustrator, designer, and storyteller. I currently live in Austin, TX with my wife and two cats.</p>
+                            <p>I have been working professionally since 2005. The fields have run the gamut: from online games and independent film, to marketing campaigns, spectacle theater, and AAA videogames. I hope to retain my own voice with each client I work for, creating a humorous but meaningful experience for all people.</p>
+                            <p>I am currently studying to be a full stack developer.</p>
+                        </article>
+                    </div>
+                    <div class="sidebar">
+                        <aside>
+                            <h2>Connect with Me</h2>
+                            <hr />
+                            <div class="u-full-width">
+                                <div class="connect"><a href="https://github.com/roblovelett/"><img src="assets/images/GitHub-Mark-120px-plus.png" alt="GitHub"/></a></div>
+                                <div class="connect"><a href="https://linkedin.com/in/roblovelett/"><img src="assets/images/In-2C-128px-R.png" alt="LinkedIn" /></a></div>
+                                <div class="connect"><a href="https://stackoverflow.com/users/6554033"><img src="assets/images/so-icon.png" alt="Stack Overflow" /></a></div>
+                                <div class="u-cf"></div>
+                            </div>
+                        </aside>
+                    </div>
+                </div>
+            </div>  
+        </main>
+    </div>
+    <footer>
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div>
+                        &copy; Copyright 2016 Robert Lovelett
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
