c273c2d1826627452d3e068ee6f2948978433951
Rob Lovelett <rlovelett@gmail.com>
2016-11-02 15:57:32 -0500

    cleaned up index.php, css

--- index.php
+++ index.php
@@ -27,35 +27,32 @@
         </div>
     </header>
     <main>
-        <div class="content">
-            <div class="container">
-                <div class="row">
-                    <div class="main">
-                        <article>
-                            <h1>About Me</h1>
-                            <hr />
-                            <img src="assets/images/profile.png" alt="Robert Lovelett" class="profile" />
-                            <p>My name is Robert Lovelett and I am an animator, illustrator, designer, and storyteller. I currently live in Austin, TX with my wife and two cats.</p>
-                            <p>I have been working professionally since 2005. The fields have run the gamut: from online games and independent film, to marketing campaigns, spectacle theater, and AAA videogames. I hope to retain my own voice with each client I work for, creating a humorous but meaningful experience for all people.</p>
-                            <p>I am currently studying to be a full stack developer.</p>
-                        </article>
-                    </div>
-                    <div class="sidebar">
-                        <aside>
-                            <h2>Connect with Me</h2>
-                            <hr />
-                            <div class="u-full-width">
-                                <div class="connect"><a href="https://github.com/roblovelett/"><img src="assets/images/GitHub-Mark-120px-plus.png" alt="GitHub"/></a></div>
-                                <div class="connect"><a href="https://linkedin.com/in/roblovelett/"><img src="assets/images/In-2C-128px-R.png" alt="LinkedIn" /></a></div>
-                                <div class="connect"><a href="https://stackoverflow.com/users/6554033"><img src="assets/images/so-icon.png" alt="Stack Overflow" /></a></div>
-                                <div class="u-cf"></div>
-                            </div>
-                        </aside>
-                    </div>
+        <div class="container">
+                <div class="main">
+                    <article>
+                        <h1>About Me</h1>
+                        <hr />
+                        <img src="assets/images/profile.png" alt="Robert Lovelett" class="profile" />
+                        <p>My name is Robert Lovelett and I am an animator, illustrator, designer, and storyteller. I currently live in Austin, TX with my wife and two cats.</p>
+                        <p>I have been working professionally since 2005. The fields have run the gamut: from online games and independent film, to marketing campaigns, spectacle theater, and AAA videogames. I hope to retain my own voice with each client I work for, creating a humorous but meaningful experience for all people.</p>
+                        <p>I am currently studying to be a full stack developer.</p>
+                    </article>
                 </div>
-            </div>  
-        </main>
-    </div>
+                <div class="sidebar">
+                    <aside>
+                        <h2>Connect with Me</h2>
+                        <hr />
+                        <div class="u-full-width">
+                            <div class="connect"><a href="https://github.com/roblovelett/"><img src="assets/images/GitHub-Mark-120px-plus.png" alt="GitHub"/></a></div>
+                            <div class="connect"><a href="https://linkedin.com/in/roblovelett/"><img src="assets/images/In-2C-128px-R.png" alt="LinkedIn" /></a></div>
+                            <div class="connect"><a href="https://stackoverflow.com/users/6554033"><img src="assets/images/so-icon.png" alt="Stack Overflow" /></a></div>
+                            <div class="u-cf"></div>
+                        </div>
+                    </aside>
+                </div>
+                <div class="u-cf"></div>
+        </div>  
+    </main>
     <footer>
         <div class="footer">
             <div class="container">
--- portfolio.php
+++ portfolio.php
@@ -1,93 +1,47 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Basic Portfolio: Robert Lovelett</title>
-<link rel="stylesheet" type="text/css" href="assets/css/reset.css">
-<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <title>Basic Portfolio: Robert Lovelett</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <div class="header u-full-width u-cf">
-        <div class="container">
-            <div class="row">
-                <div class="header-name">
-                    Robert Lovelett
-                </div>
-                <div class="header-nav">
-                    <nav>
-                        <ul>
-                            <li><a href="index.php">About</a></li>
-                            <li><a href="portfolio.php">Portfolio</a></li>
-                            <li><a href="contact.php">Contact</a></li>
-                        </ul>
-                    </nav>
+    <header>
+        <div class="header">
+            <div class="container">
+                <div class="row">
+                    <div class="header-name">Robert Lovelett</div>
+                    <div class="header-nav">
+                        <nav>
+                            <ul>
+                                <li><a href="index.php">About</a></li>
+                                <li><a href="portfolio.php">Portfolio</a></li>
+                                <li><a href="contact.php">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="u-cf"></div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="content">
-        <main>
+    </header>
+    <main>
+        <div class="content">
             <div class="container">
                 <div class="row">
                     <div class="main">
-                        <h1>Portfolio</h1>
-                        <hr />
-                        <div class="container">
-                            <div class="row">
-                                <div class="portfolio">
-                                    <a href="#">
-                                        <img src="assets/images/thumbnail01.jpg" alt="eyyyy" />
-                                        <div class="portfolio-text">
-                                            Project 01
-                                        </div>
-                                    </a>
-                                </div>
-                               <div class="portfolio">
-                                    <a href="#">
-                                        <img src="assets/images/thumbnail02.jpg" alt="lmao" />
-                                        <div class="portfolio-text">
-                                            Project 02
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="portfolio">
-                                    <a href="#">
-                                        <img src="assets/images/thumbnail03.jpg" alt="rofl" />
-                                        <div class="portfolio-text">
-                                            Project 03
-                                        </div>
-                                    </a>
-                                </div>
-                               <div class="portfolio">
-                                    <a href="#">
-                                        <img src="assets/images/thumbnail04.jpg" alt="lul" />
-                                        <div class="portfolio-text">
-                                            Project 04
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="portfolio">
-                                    <a href="#">
-                                        <img src="assets/images/thumbnail05.jpg" alt="!" />
-                                        <div class="portfolio-text">
-                                            Project 05
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
+                        
                     </div>
                     <div class="sidebar">
                         <aside>
                             <h2>Connect with Me</h2>
                             <hr />
-                            <div class="connect">
-                                <a href="https://github.com/roblovelett/"><img src="assets/images/GitHub-Mark-120px-plus.png" alt="GitHub"/></a>
-                                <a href="https://linkedin.com/in/roblovelett/"><img src="assets/images/In-2C-128px-R.png" alt="LinkedIn" /></a>
-                                <a href="https://stackoverflow.com/users/6554033"><img src="assets/images/so-icon.png" alt="Stack Overflow" /></a>
+                            <div class="u-full-width">
+                                <div class="connect"><a href="https://github.com/roblovelett/"><img src="assets/images/GitHub-Mark-120px-plus.png" alt="GitHub"/></a></div>
+                                <div class="connect"><a href="https://linkedin.com/in/roblovelett/"><img src="assets/images/In-2C-128px-R.png" alt="LinkedIn" /></a></div>
+                                <div class="connect"><a href="https://stackoverflow.com/users/6554033"><img src="assets/images/so-icon.png" alt="Stack Overflow" /></a></div>
+                                <div class="u-cf"></div>
                             </div>
                         </aside>
                     </div>
@@ -96,7 +50,7 @@
         </main>
     </div>
     <footer>
-        <div class="footer u-full-width u-cf">
+        <div class="footer">
             <div class="container">
                 <div class="row">
                     <div>
