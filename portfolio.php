c3858cc0dcdc605f34607684625380dea4833f7d
Rob Lovelett <rlovelett@gmail.com>
2016-11-03 15:12:02 -0500

    made portfolio responsive

--- portfolio.php
+++ portfolio.php
@@ -27,28 +27,69 @@
         </div>
     </header>
     <main>
-        <div class="content">
-            <div class="container">
-                <div class="row">
-                    <div class="main">
-                        
+        <div class="container">
+            <div class="main">
+                <h1>Portfolio</h1>
+                <hr />
+                <a href="#">
+                    <div class="portfolio">
+                        <img class="portfolio-img" src="assets/images/thumbnail01.jpg" alt="" />
+                        <div class="portfolio-title u-full-width">
+                            Project 01
+                        </div>
                     </div>
-                    <div class="sidebar">
-                        <aside>
-                            <h2>Connect with Me</h2>
-                            <hr />
-                            <div class="u-full-width">
-                                <div class="connect"><a href="https://github.com/roblovelett/"><img src="assets/images/GitHub-Mark-120px-plus.png" alt="GitHub"/></a></div>
-                                <div class="connect"><a href="https://linkedin.com/in/roblovelett/"><img src="assets/images/In-2C-128px-R.png" alt="LinkedIn" /></a></div>
-                                <div class="connect"><a href="https://stackoverflow.com/users/6554033"><img src="assets/images/so-icon.png" alt="Stack Overflow" /></a></div>
-                                <div class="u-cf"></div>
-                            </div>
-                        </aside>
+                </a>
+                <a href="#">
+                    <div class="portfolio">
+                        <img class="portfolio-img" src="assets/images/thumbnail02.jpg" alt="" />
+                        <div class="portfolio-title u-full-width">
+                            Project 02
+                        </div>
                     </div>
-                </div>
-            </div>  
-        </main>
-    </div>
+                </a>
+                <div class="u-cf"></div>
+                <a href="#">
+                    <div class="portfolio">
+                        <img class="portfolio-img" src="assets/images/thumbnail03.jpg" alt="" />
+                        <div class="portfolio-title u-full-width">
+                            Project 03
+                        </div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="portfolio">
+                        <img class="portfolio-img" src="assets/images/thumbnail04.jpg" alt="" />
+                        <div class="portfolio-title u-full-width">
+                            Project 04
+                        </div>
+                    </div>
+                </a>
+                <div class="u-cf"></div>
+                <a href="#">
+                    <div class="portfolio">
+                        <img class="portfolio-img" src="assets/images/thumbnail05.jpg" alt="" />
+                        <div class="portfolio-title u-full-width">
+                            Project 05
+                        </div>                            
+                    </div>
+                </a>
+                <div class="u-cf"></div>
+            </div>
+            <div class="sidebar">
+                <aside>
+                    <h2>Connect with Me</h2>
+                    <hr />
+                    <div class="u-full-width">
+                        <div class="connect"><a href="https://github.com/roblovelett/"><img src="assets/images/GitHub-Mark-120px-plus.png" alt="GitHub"/></a></div>
+                        <div class="connect"><a href="https://linkedin.com/in/roblovelett/"><img src="assets/images/In-2C-128px-R.png" alt="LinkedIn" /></a></div>
+                        <div class="connect"><a href="https://stackoverflow.com/users/6554033"><img src="assets/images/so-icon.png" alt="Stack Overflow" /></a></div>
+                        <div class="u-cf"></div>
+                    </div>
+                </aside>
+            </div>
+            <div class="u-cf"></div>
+        </div>  
+    </main>
     <footer>
         <div class="footer">
             <div class="container">
